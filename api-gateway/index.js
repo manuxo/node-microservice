45bd3167513c06ddda2fc1ba553e7343b19b4f04
Manuel Alvarado Estanga <alvarado.manuel@live.com>
2022-07-25 23:19:48 -0500

    add rabbitmq demo

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const httpProxy = require('express-http-proxy')
+const httpProxy = require('express-http-proxy');
+let amqp = require('amqplib/callback_api');
 const PORT = process.env.PORT || 3000;
+const RABBITMQ_HOST = process.env.RABBITMQ_HOST || "localhost";
 
 const app = express();
 
@@ -27,5 +29,30 @@ app.get("/service1", serviceOneProxy);
 app.get("/service2", serviceTwoProxy);
 
 app.get("/service3", serviceThreeProxy);
+
+app.get("/messages", (req, res, next) => {
+    const { msg } = req.query;
+    if (msg) {
+        amqp.connect(`amqp://${RABBITMQ_HOST}`, (error0, conn) => {
+            if (error0) {
+                throw error0;
+            }
+            conn.createChannel((error1, channel) => {
+                if (error1) {
+                    throw error1;
+                }
+                var queueName = 'message-box';
+                channel.assertQueue(queueName, {
+                    durable: false
+                });
+                channel.sendToQueue(queueName, Buffer.from(msg));
+                console.log("API Gateway sent %s", msg);
+                res.status(201).send("Message sent successfully");
+            });
+        });
+    } else {
+        res.status(400).send("Message is empty");
+    }
+});
 
 app.listen(PORT, () => console.log(`API Gateway is listening on port ${PORT}`));
--- service/index.js
+++ service/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+let amqp = require('amqplib/callback_api');
 const SERVICE_ID = process.env.SERVICE_ID || "local";
+const RABBITMQ_HOST = process.env.RABBITMQ_HOST || "localhost";
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -9,6 +11,30 @@ app.use(express.json());
 
 app.get(`/${SERVICE_ID}`, (req, res, next) => {
     res.status(200).send(`Response from service ${SERVICE_ID} retrieved successfully.`);
+});
+
+amqp.connect(`amqp://${RABBITMQ_HOST}`, (error0, conn) => {
+    if (error0) {
+        console.error(error0);
+        throw error0;
+    }
+    conn.createChannel((error1, channel) => {
+        if (error1) {
+            console.error(error1);
+            throw error1;
+        }
+
+        const queue = 'message-box';
+
+        channel.assertQueue(queue, {
+            durable: false
+        });
+
+        console.log(`${SERVICE_ID} waiting for messages in queue: ${queue}.`);
+        channel.consume(queue, msg => {
+            console.log(`${SERVICE_ID} received ${msg.content.toString()}`);
+        });
+    });
 });
 
 app.listen(PORT, () => console.log(`${SERVICE_ID} is listening on port ${PORT}`));
